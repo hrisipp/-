2a46a6a8058609023db2be1b90701e3f2acd0439
hrisipp <slavakostinenko@yandex.ru>
2024-06-09 15:07:47 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+#Задача «Длина слова»
+a = "а я думала сова"
+lenA = len(a)
+print(lenA)
+#Задача «Суммы и разности»
+first = 13
+second = 5
+summa = first + second
+diff = first - second
+print(summa , diff)
+#Задача «Среднее арифметическое»(возьму одно дробное(просто так))
+first = 15
+second = 5
+third = 13.0
+mean = (first + second + third)/3
+print(mean)
+#Задача «Простые строчки»
+first_string = "Вторник"
+second_string = "Понедельник"
+print(second_string,", ",first_string)
+#Задача «Сложная формула»
+a = 1
+b = 2
+c = 3
+f = (((a * b) + (a * c)) ** 3)/2
+print(f)
